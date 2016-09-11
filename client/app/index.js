ade3e4590b7bb21424d3be111040aebbcd0e20c1
Rodrigo Juliani <srodriki@gmail.com>
2016-09-11 15:04:33 -0300

    #1 Updated project scaffolding so that it can build correctly using brunch

---
+++ client/app/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './config/routes'
+import VueResource from 'vue-resource'
+import VueValidator from 'vue-validator'
+import App from './components/app.vue'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+Vue.use(VueValidator)
+
+
+const environment = process.env.NODE_ENV
+
+Vue.config.debug = (environment != 'production')
+Vue.config.devtools = (environment != 'production')
+
+// Initialize Vue Router
+let router = new VueRouter({
+    history: true
+})
+
+router.map(routes)
+
+// Start the application
+router.start(App, '#content', function() {
+
+    if (window) {
+        window.App = router.app;
+    }
+
+})
