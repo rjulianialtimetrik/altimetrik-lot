d0cbccf4c1c8204e2fb626210b8b1f4d5a1f370a
Rodrigo Juliani <srodriki@gmail.com>
2016-09-15 06:53:33 -0300

    list of questions can now be filtered by the question's text

--- client/app/components/questions/list.vue
+++ client/app/components/questions/list.vue
@@ -6,8 +6,20 @@
               <i class="fa fa-plus"></i>
           </a>
       </h1>
+      <div class="wrapper mt-xl">
+          <form class="form-horizontal col-xs-12" >
+              <div class="form-group">
+                  <div class="input-group">
+                     <input type="text" class="form-control" v-model="filter">
+                     <span class="input-group-btn">
+                        <button type="button" class="btn btn-default"><i class="fa fa-search"></i></button>
+                     </span>
+                  </div>
+              </div>
+          </form>
+      </div>
       <div class="mt-xl row-masonry row-masonry-xl-3 row-masonry-lg-3 row-masonry-md-3 row-masonry-sm-2">
-          <div class="col-masonry" v-for.sync="q in questions">
+          <div class="col-masonry" v-for.sync="q in questions | filterBy filter in 'text'">
               <question :question="q" v-on:delete-question="deleteQuestion"></question>
           </div>
       </div>
@@ -20,7 +32,8 @@ import Question from './question.vue'
 export default {
   data() {
     return {
-        questions: []
+        questions: [],
+        filter: ""
     };
   },
   computed: {},
